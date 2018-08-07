4b76b4def81fd6726a50daf5c98a63cfbe573d7f
WaytKu <jahu.ku@wayties.com>
2018-08-07 22:15:54 +0900

    Language Fix

--- js/GF.js
+++ js/GF.js
@@ -1351,6 +1351,23 @@ function loadLanguage(){
             });
             break;
         default:
+            langPack = langPacks.en;
+            Highcharts.setOptions({
+                lang: {
+                    months: [
+                        'January','February','March','April','May','June','July','August','September','October','November','December'
+                    ],
+                    shortMonths: [
+                        'Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.'
+                    ],
+                    weekdays: [
+                        'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'
+                    ],
+                    shortWeekdays: [
+                        'Sun.','Mon.','Tue.','Wed.','Thu.','Fri.','Sat.'
+                    ]
+                }
+            });
             break;
     }
     setLanguage();
