3dba85e7c1695ddc9640a3b23c4f212c3e37fc2b
WaytKu <jahu.ku@wayties.com>
2018-08-29 22:53:12 +0900

    UI
    버그 수정

--- js/GF.js
+++ js/GF.js
@@ -1425,13 +1425,13 @@ function loadLanguage(){
                         'January','February','March','April','May','June','July','August','September','October','November','December'
                     ],
                     shortMonths: [
-                        'Jan.','Feb.','Mar.','Apr.','May','Jun.','Jul.','Aug.','Sep.','Oct.','Nov.','Dec.'
+                        'Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'
                     ],
                     weekdays: [
                         'Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'
                     ],
                     shortWeekdays: [
-                        'Sun.','Mon.','Tue.','Wed.','Thu.','Fri.','Sat.'
+                        'Sun','Mon','Tue','Wed','Thu','Fri','Sat'
                     ]
                 }
             });
