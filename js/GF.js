10bb06b5a36138228681b04f186fb9fe0931c484
WaytKu <jahu.ku@wayties.com>
2018-11-08 18:04:21 +0900

    debug

--- js/GF.js
+++ js/GF.js
@@ -3,7 +3,7 @@ var updateString    = "2018-10-28 Changelog"
                     + "\n- 군수 시간표(베타) 기능 추가"
                     ;
 
-var selLang         = 'ko';   //기본 언어는 한
+var selLang         = 'en';   //기본 언어는 한
 var langPacks;
 var langPack;
 
